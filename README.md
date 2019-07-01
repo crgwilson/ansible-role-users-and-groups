33ebea4b3c8c3bf7fbdd0a598754202e21bbd741
Craig Wilson <craigjwilson93@gmail.com>
2019-06-30 21:48:36 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,50 @@
+# Ansible role: Users and Groups
+
+Create arbitrary users and groups
+
+* Create user groups
+* Create users
+* Insert SSH keys into user home directories
+* Create user authorized keys
+
+## Variables
+
+### users_and_groups_groups
+
+Dictionary defining groups to create
+
+Example:
+
+```yaml
+users_and_groups_groups:
+  group1:
+    state: present
+  group2:
+    state: absent
+```
+
+### users_and_groups_users
+
+Dictionary defining users to create
+
+Example:
+
+```yaml
+users_and_groups_users:
+  bob:
+    shell: /bin/bash
+    ssh_key: bobs_key.pem # file path for lookup
+    authorized_key: bobs_authorized_key # file path for lookup
+    groups:
+      - bobs_group
+      - sudo
+```
+
+## Testing
+
+Unit tests for this project are setup using [Molecule](https://molecule.readthedocs.io/en/stable/) & [Docker](https://www.docker.com/).
+Tests can be run using the below command:
+
+```console
+foo@bar:~$ molecule test --all
+```
---
+++ molecule/default/tests/test_default.py
@@ -0,0 +1,52 @@
+import os
+import pytest
+
+import testinfra.utils.ansible_runner
+
+testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
+    os.environ['MOLECULE_INVENTORY_FILE']).get_hosts('all')
+
+
+@pytest.mark.parametrize('group, exists', [
+    ('sudo', True),
+    ('testaddgroup', True),
+    ('testdeletegroup', False)
+])
+def test_default_user_groups(host, group, exists):
+    g = host.group(group)
+    assert g.exists == exists
+
+
+@pytest.mark.parametrize('user, groups, shell', [
+    ('testbashuser', ['testbashuser', 'sudo', 'testaddgroup'], '/bin/bash'),
+    ('testcshuser', None, '/bin/csh')
+])
+def test_default_users(host, user, groups, shell):
+    u = host.user(user)
+    assert u.group == user
+    assert u.shell == shell
+    assert u.home == '/home/' + user
+    if groups:
+        assert u.groups == groups
+
+    d = host.file('/home/' + user + '/.ssh')
+    assert d.is_directory
+    assert d.user == user
+    assert d.group == user
+    assert d.mode == 0700
+
+
+def test_default_ssh_key(host):
+    f = host.file('/home/testbashuser/.ssh/test')
+    assert f.is_file
+    assert f.user == 'testbashuser'
+    assert f.group == 'testbashuser'
+    assert f.mode == 0600
+
+
+def test_default_authorized_keys(host):
+    d = host.file('/home/testbashuser/.ssh/authorized_keys')
+    assert d.is_file
+    assert d.user == 'testbashuser'
+    assert d.group == 'testbashuser'
+    assert d.mode == 0600
